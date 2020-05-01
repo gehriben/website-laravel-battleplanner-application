e1db01618e1b06382650647e0e5b0b142c0a4efa
Jared Snape <jaredsnape@gmail.com>
2020-05-01 17:17:24 -0400

    Finished index and room css

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -6,90 +6,14 @@
 
 @push('css')
   <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
-  <!-- <link rel="stylesheet" href="{{asset("css/index/index.css")}}"> -->
   <style>
       body{
-        background-color: black;
-        background-image: url("https://battleplanner-production.s3.ca-central-1.amazonaws.com/static/R6S.jpg");
+        background-color: white;
+        /* background-image: url("https://battleplanner-production.s3.ca-central-1.amazonaws.com/static/R6S.jpg"); */
       }
-      .card{
-        max-height: 400px;
-        margin:10px;
-        background-color:grey;
-        cursor: pointer;
-      }
-      
-      .card:hover{
-        background-color: lightgrey;
-      }
-
-      .card-text{
-        color: #36ff36;
-        -webkit-text-stroke: 1px black;
-        font-size: 2em;
-        font-family: 'Fredoka One', cursive;
-      }
-
-
   </style>
 @endpush
 
 @section('content')
-<div class="container">
-    <div class="row">
-
-        <div class="col-md">
-
-            <div class="card">
-                <!-- <img src="https://via.placeholder.com/150x70" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <p class="card-text text-center">Profile</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-md">
-
-            <div class="card">
-                <!-- <img src="https://via.placeholder.com/150x70" class="card-img-top" alt="..."> -->
-                <a href="/battleplan/new">
-                    <div class="card-body">
-                        <p class="card-text text-center">Start Planning</p>
-                    </div>
-                </a>
-            </div>
-
-        </div>
-
-    </div>
-
-    <div class="row">
-
-        <div class="col-md">
-
-            <div class="card">
-                <!-- <img src="https://via.placeholder.com/150x70" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <p class="card-text text-center">Rooms</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-md">
-
-            <div class="card">
-                <!-- <img src="https://via.placeholder.com/150x70" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <p class="card-text text-center">Public Plans</p>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-</div>
-
 
 @endsection
--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -12,58 +12,6 @@
 
 @push('css')
   <link rel="stylesheet" href="{{asset("css/room/index.css")}}">
-
-  <style>
-    .jumbotron{
-      margin: auto;
-      margin-top: 15px;
-    }
-
-    .fas{
-      font-size: 100px;
-    }
-
-    .card {
-      margin: auto;
-      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-      transition: 0.3s;
-      width: 40%;
-      height:300px;
-      border-radius: 25px;
-    }
-
-    .alert{
-      border-radius: 25px;
-    }
-
-    .new-room:hover {
-      background-color: #dee0de;
-    }
-
-    .new-room .fas{
-      color: green;
-    }
-
-    .new-room .container{
-      margin-top: 15%;
-    }
-
-    .join-room .fas{
-      color: blue;
-    }
-
-    .new-room{
-      cursor: pointer;
-    }
-
-    .new-room:hover {
-      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-    .head{
-      background-color: white;
-      border-radius: 25px;
-    }
-  </style>
 @endpush
 
 @section('content')
@@ -76,38 +24,21 @@
         @endif
     </div>
     <div class="row mt-3">
-
-      <div class="head text-center">
-          <div class="text-center">
-            <h1>Room Options</h1>
-            <h5>Rooms are used to create battleplans. Any number of people can
-                be connected to a single Room and any changes the Room's owner
-                makes will appear for everyone else, allowing fluid planning and
-                easy switching between all of your saved battleplans.
-            </h5>
-          </div>
-        </div>
-        
         <div data-link="/room/create" class="card new-room mt-2 text-center">
           <div class="container">
             <i class="fas fa-user-alt"></i>
-            <h4><b>New Room</b></h4> 
-            <p>Host your own room</p> 
+            <h4><b>Host a New Room</b></h4>
           </div>
         </div>
-
 
         <div data-link="/room/join" class="card join-room mt-2 text-center">
           <div class="container">
             <i class="fas fa-user-friends"></i>
-            <h4><b>Join Room</b></h4> 
-            <p>Join existing room</p> 
-            <input type="text" class="form-control text-center" placeholder="room #"id="room">
-            <button type="button" onclick="join( $('#room').val() )" class="col-12 btn btn-success">Join</button>
+            <h4><b>Join an Existing Room</b></h4>
+            <input type="text" class="form-control text-center" placeholder="Room #"id="room">
+            <button type="button" onclick="join( $('#room').val() )" class="btn btn-success">Join</button>
           </div>
         </div>
-
-      
     </div>
   </div>
 @endsection
